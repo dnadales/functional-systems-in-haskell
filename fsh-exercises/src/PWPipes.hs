e8c5585d676d91448f7e027025526813d4e0b1d4
Damian Nadales <damian.nadales@gmail.com>
2017-05-13 11:09:49 +0200

    Added some notes on `Consumer`s.

--- fsh-exercises/src/PWPipes.hs
+++ fsh-exercises/src/PWPipes.hs
@@ -3,9 +3,11 @@
 
 module PWPipes where
 
-import           Control.Monad (unless)
+import           Control.Exception (throwIO, try)
+import           Control.Monad     (unless)
+import qualified GHC.IO.Exception  as G
 import           Pipes
-import           System.IO     (isEOF)
+import           System.IO         (isEOF)
 
 -- * Producers
 
@@ -40,7 +42,7 @@ runStdIn = runEffect stdinReader
 tripleP :: a -> Producer a IO ()
 tripleP x = yield x >> yield x >> yield x
 
--- Can we use @tripleP@ to write an effects that triples the lines enteres in
+-- Can we use @tripleP@ to write an effects that triples the lines entered in
 -- the standard input?
 --
 -- Not with producers only. We need ways to pass the value yielded by @stdinLn@
@@ -71,3 +73,52 @@ stdinRedaer3' :: Effect IO ()
 stdinRedaer3' = for stdinLn (reverseP ~> tripleP ~> lift . putStrLn)
 
 -- * Consumers
+
+-- | A consumer that awaits for strings.
+stdoutLn :: Consumer String IO ()
+stdoutLn = do
+  str <- await -- await a String
+  x <- lift $ try $ putStrLn str
+  case x of
+    Left e@(G.IOError { G.ioe_type = t}) ->
+      lift $ unless (t == G.ResourceVanished) $ throwIO e
+    Right _ -> stdoutLn
+
+-- How do we feed this consumer? We use @>~@ (feed).
+stdoutWriter :: IO ()
+stdoutWriter = runEffect $ lift readLn >~ stdoutLn
+-- Careful when testing this. You need to input strings, which are delimited by
+-- @"@s. At least from the ghci REPL. If you forget to quote them you'd get a
+--
+-- > *** Exception: user error (Prelude.readIO: no parse)
+--
+-- error.
+
+-- Writing a consumer that consumes two times
+doubleC :: Monad m => Consumer a m (a, a)
+doubleC = (,) <$> await <*> await
+
+-- And a consumer that consumes a pair of lists and concatenates them.
+concatPairC :: Monad m => Consumer ([a], [a]) m [a]
+concatPairC = (uncurry (++)) <$> await
+  -- do
+  -- (xs, ys) <- await
+  -- return (xs ++ ys)
+
+-- And lets put everything together:
+stdoutDoubleWriter :: IO ()
+stdoutDoubleWriter = runEffect $ lift readLn >~ doubleC >~ concatPairC >~ stdoutLn
+
+-- Once @doubleC@ outputs something, this is passed to the @await@s of other consumers.
+--
+-- Note the difference between the way consumers and producers are composed.
+-- The output of the consumers are passed to the other consumers using @>~@,
+-- but in case of the producers, they don't consume any input, but producers
+-- have to be created by a function @a -> Producer b m c@. Compare the types of
+-- both functions:
+--
+-- > (~>) :: Monad m => (a -> Producer b m ()) -> (b -> Producer c m ()) -> (a -> Producer c m ())
+-- > (>~) :: Monad m => Consumer a m b         -> Consumer b m c         -> Consumer a m c
+--
+
+-- * Pipes
